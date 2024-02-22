1382cb54dd24c05af6d63e949a0019938ab8a1f2
Jae Young Seo <jaeyoungseo@jys.local>
2024-02-21 19:28:51 -0500

    visual representation of bitboards

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import boards.Bitboards;
+
 public class Main {
+
 
     public static void main(String[] args) {
         System.out.println("this is going to be a super cool chess program");
+        Bitboards game = new Bitboards();
+        System.out.println(game);
     }
 }
